730481b7540ed767437363743aa5d57d2fd30758
Documenter.jl <documenter@juliadocs.github.io>
2023-08-01 08:52:54 +0000

    build based on 4c7c47f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MetaRange","category":"page"},{"location":"#MetaRange","page":"Home","title":"MetaRange","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MetaRange.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MetaRange]","category":"page"}]
+}
