e0faf3750dbbded200de5b19b92533f8187a1b92
janablechschmidt <jana.blechschmidt@uni-wuerzburg.de>
2023-08-02 14:55:31 +0200

    Added Dependencies and exports of the module

--- src/MetaRange.jl
+++ src/MetaRange.jl
@@ -1,5 +1,29 @@
 module MetaRange
 
-# Write your package code here.
+using CSV
+using DataFrames
+using Dates
+using DelimitedFiles
+using Distributions
+using Random
 
+#main module functions
+export read_input
+export Run_Simulation!
+
+#main module struct
+export Simulation_Data
+
+#examples and defaults
+export default_run_data
+
+include("datastructures.jl")
+include("datastructure_constructors.jl")
+include("read_input.jl")
+include("initialization_functions.jl")
+include("eco_loop.jl")
+include("eco_functions.jl")
+include("eco_loop_functions.jl")
+include("defaults.jl")
+include("default_constants.jl")
 end
