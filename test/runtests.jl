0beac2c9e0dda568a60e1c7896a0e432c8878c19
rroelz <robin.roelz@gmail.com>
2023-08-25 17:34:21 +0200

    added mostly empty tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,7 @@ using Test
 @testset "MetaRange.jl" begin
     include("constants.jl")
     include("eco_functions.jl")
+    include("eco_loop_functions.jl")
+    include("read_input.jl")
     include("initialization_functions.jl")
 end
