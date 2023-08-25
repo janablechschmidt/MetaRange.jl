d447106904d99fae2da2e377b1ba733b2f7cf81e
rroelz <robin.roelz@gmail.com>
2023-08-25 17:09:28 +0200

    updated testset

--- test/initialization_functions.jl
+++ test/initialization_functions.jl
@@ -1,5 +1,5 @@
 @testset "Initialization" begin
-    @testset "randomize()" begin
+    @testset "randomize!()" begin
         Random.seed!(10)
         @testset "standard deviation" begin
             @test randomize!(10.0, 0.0) == 10.0
@@ -9,15 +9,16 @@
             @test randomize!(m, 0.1) != m
         end
         @testset "zero" begin
+            @test randomize!(0.0, 0.1) == 0.0
             m = zeros(Float64, 10, 10)
             @test randomize!(m, 0.0) == m
             @test randomize!(m, 0.1) == m
         end
         @testset "missing values" begin
-            @test randomize!(missing, 0.0) === missing
+            @test randomize!(missing, 0.2) === missing
         end
         @testset "NaNs" begin
-            @test randomize!(NaN, 0.0) === NaN #return NaNs when read in for
+            @test randomize!(NaN, 0.2) === NaN #return NaNs when read in for
         end
     end
 end
