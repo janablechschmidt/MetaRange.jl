fdb02d028bdb007be2457f69159a71eac1e48561
rroelz <robin.roelz@gmail.com>
2023-08-28 10:08:30 +0200

    Add testfiles for all files in `src/`

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,12 @@ using Test
 
 @testset "MetaRange.jl" begin
     include("constants.jl")
+    include("datastructure_constructors.jl")
+    include("datastructures.jl")
+    include("defaults.jl")
     include("eco_functions.jl")
     include("eco_loop_functions.jl")
-    include("read_input.jl")
+    include("eco_loop.jl")
     include("initialization_functions.jl")
+    include("read_input.jl")
 end
