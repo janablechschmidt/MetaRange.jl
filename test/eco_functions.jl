ffc1a11c0d9a462112003f0da5d85b12e9b66a4e
rroelz <robin.roelz@gmail.com>
2023-08-28 12:12:58 +0200

    Added some more tests

--- test/eco_functions.jl
+++ test/eco_functions.jl
@@ -1,17 +1,29 @@
 @testset "eco_functions.jl" begin
-    #initialise tuples for testing
     Random.seed!(10)
-    Ns = rand(1:100000, 100)
-    g = rand(0.01:0.01:10.0, 100)
-    c = rand(0.0:0.01:100000.0, 100)
-    param = [(Ns[i], g[i], c[i], nothing) for i=1:100]
-    @testset "Ricker" begin
-        @testset "known cases" begin
-            @test MetaRange.ReproductionRicker(100, 1.0, 100.0, nothing) == 100
-            @test MetaRange.ReproductionRicker(100, 0.0, 200.0, nothing) == 100
+    @testset "reproduction" begin
+        Ns = rand(1:100000, 10)
+        g = rand(0.01:0.01:10.0, 10)
+        c = rand(0.0:0.01:100000.0, 10)
+        param = [(Ns[i], g[i], c[i], nothing) for i=1:10]
+        @testset "Ricker" begin
+            @testset "known cases" begin
+                @test MetaRange.ReproductionRicker(100, 1.0, 100.0, nothing) == 100
+                @test MetaRange.ReproductionRicker(100, 0.0, 200.0, nothing) == 100
+            end
+            @testset "growth" for p in param
+                @test xor(MetaRange.ReproductionRicker(p...) > p[1], p[1] > p[3])
+            end
         end
-        @testset "growth" for p in param
-            @test xor(MetaRange.ReproductionRicker(p...) > p[1], p[1] > p[3])
+        @testset "Beverton" begin
+            @testset "known cases" begin
+                @test MetaRange.ReproductionBeverton(100, 1.0, 100.0, 1.0) == 100
+                @test isnan(MetaRange.ReproductionBeverton(100, 0.0, 200.0, 1.0))
+            end
+            @testset "growth" begin
+                n = MetaRange.ReproductionBeverton(100, 1.0, 200.0, 1.0)
+                @test n < MetaRange.ReproductionBeverton(100, 2.0, 200.0, 1.0)
+                @test n > MetaRange.ReproductionBeverton(100, 0.5, 200.0, 1.0)
+            end
         end
     end
 end
