43b95f6cd05abbc16714054f50d2d7b8e943f964
rroelz <robin.roelz@gmail.com>
2023-09-28 13:35:13 +0200

    Add test for path extension and moved calibration

--- test/initialization_functions.jl
+++ test/initialization_functions.jl
@@ -21,4 +21,10 @@
             @test MetaRange.randomize!(NaN, 0.2) === NaN #return NaNs when read in for
         end
     end
+    @testset "ParamCalibration" begin
+        @test MetaRange.ParamCalibration(0.0, 1.0, 1.0, 297.0, 0.65) == 0
+        #test that if exponent == 1.0 mass has no effect
+        p = MetaRange.ParamCalibration(1.0, 1.0, 1.0, 297.0, 0.65)
+        @test p == MetaRange.ParamCalibration(1.0, 10.0, 1.0, 297.0, 0.65)
+    end
 end
