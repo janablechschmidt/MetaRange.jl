f787058d9f3610e5a1ffd8a2188b02d193d4f955
Jana <jana.blechschmidt@uni-wuerzburg.de>
2023-10-20 16:48:54 +0200

    need to force create out_dir if backup=f

--- src/output.jl
+++ src/output.jl
@@ -141,5 +141,6 @@ function save_all(SD::Simulation_Data)
     carry_out = hcat(t, x, y, carry, repeat(["carry"], length(t)))
     bevmort_out = hcat(t, x, y, bevmort, repeat(["bevmort"], length(t)))
     out = vcat(abundance_out, habitat_out, reproduction_out, carry_out, bevmort_out)
+    make_out_dir(SD.parameters.output_dir)
     writedlm(joinpath(SD.parameters.output_dir, "output.csv"), out)
 end
